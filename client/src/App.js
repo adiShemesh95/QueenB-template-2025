be110264528ea1c90f062fe09d40b79ea530f4d9
adiShemesh95 <adi1995sh@gmail.com>
2026-09-03 22:11:52 +0300

    Add mentee matching frontend MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import theme from "./theme";
 import Home from "./components/home/Home";
 import AuthPlaceholder from "./components/home/AuthPlaceholder";
 import Dashboard from "./components/Dashboard";
+import MyRequestsPage from "./matching/MyRequestsPage";
+import RequestDetailsPage from "./matching/RequestDetailsPage";
+import { MatchingLanguageProvider } from "./matching/MatchingLanguageContext";
+
+function MatchingRoutes() {
+  return (
+    <MatchingLanguageProvider>
+      <Outlet />
+    </MatchingLanguageProvider>
+  );
+}
 
 function App() {
   return (
@@ -16,6 +27,10 @@ function App() {
           <Route path="/register" element={<AuthPlaceholder mode="register" />} />
           <Route path="/login" element={<AuthPlaceholder mode="login" />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route element={<MatchingRoutes />}>
+            <Route path="/my-requests" element={<MyRequestsPage />} />
+            <Route path="/matching/:id" element={<RequestDetailsPage />} />
+          </Route>
         </Routes>
       </Router>
     </ThemeProvider>
